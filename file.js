cdda49ac047b403cb35de2de9c4c80c71f34122e
luchshij-Username <kagitomiryoma@gmail.com>
2024-10-24 12:15:53 +0200

    Update file.js

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
-if(document.body.section.cloud1.style.top <= 0) {
-document.body.section.img.style.top = getRndInteger(-150, -800) + 'px';
+if(document.body.section.cloud.style.top <= 0) {
+document.body.section.cloud.style.top = getRndInteger(-150, -800) + 'px';
 }
 
 
