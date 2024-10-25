6dd194fe0f0b06c5a257af6965a33a264c6e24ed
luchshij-Username <kagitomiryoma@gmail.com>
2024-10-25 09:52:17 +0200

    Update file.js

--- file.js
+++ file.js
@@ -1,4 +1,4 @@
-const imgElement = document.body.querySelector('section img');
+const imgElement = document.body.querySelector('.cloud');
 
 if (imgElement && imgElement.style.left <= '0px') {
     imgElement.style.top = getRndInteger(-150, -800) + 'px';
